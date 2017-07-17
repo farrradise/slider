d0d50175438283847e286e49b2d3f7f8419928b3
farrradise <marzini.f@gmail.com>
2017-07-17 19:46:16 +0200

    adapted the code to new pictures+ added 'p' for the legend

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 var test = document.getElementsByTagName('img');
+var test2 = document.getElementsByTagName("p");
 var index = 0;
-var image;
+var image, text;
 function a() {
-
   setInterval(leTest, 3000);
-
-
 }
 
 function leTest(){
-   if (index== 3) {
+   if (index== 4) {
      index = 0;
      image = test[index];
+     text = test2[index];
      image.style.zIndex = "2";
-     test[3].style.zIndex = "1";
-
+     text.style.zIndex = "3"
+     test[4].style.zIndex = "1";
+     test2[4].style.zIndex="-2";
    } else {
     index++;
     image = test[index];
+    text = test2[index];
     image.style.zIndex = "2";
+    text.style.zIndex = "3";
     test[index-1].style.zIndex = "1";
+    test2[index-1].style.zIndex = "-2";
    }
- } 
+ }
