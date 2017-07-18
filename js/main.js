22ac651d2ae161cbd5398644adc58cf2f762d368
farrradise <marzini.f@gmail.com>
2017-07-18 11:46:09 +0200

    test some things to have a 'control to the slider'

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ function leTest(){
     test2[index-1].style.zIndex = "-2";
    }
  }
+
+ var liste = document.getElementsByTagName('li');
+
+ for (i = 0; i<liste.length;i++) {
+   liste[i].addEventListener("click", changeZIndex(this));
+ }
+
+function changeZIndex(th) {
+  alert("test");
+ var g = document.getElementsByTagName('ul')[0];
+ for (var i = 0, len = g.children.length; i < len; i++) {
+   (function(index){
+         g.children[i].onclick = function(){
+               alert(index)  ;
+         }
+     });
+
+ }
+}
+/*
+function changeZIndex(th) {
+  var a = liste.indexOf(th);
+  alert(a);
+
+/*  switch (liste) {
+    case liste[0]:
+      // alert("ça marche");
+      break;
+    default:
+
+  }
+  */
+}
+*/
