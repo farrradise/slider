26c99858f0ed10daa3a272cfe32de6f8126644ca
farrradise <marzini.f@gmail.com>
2017-07-18 14:52:44 +0200

    add visual effect for remote contorle on focus

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var test = document.getElementsByTagName('img');
 var test2 = document.getElementsByTagName("p");
+var liste = document.getElementsByTagName('li');
 var index = 0;
 var image, text;
 function a() {
-  setInterval(leTest, 3000);
+  setInterval(leTest, 4000);
 }
 
 function leTest(){
@@ -15,6 +16,8 @@ function leTest(){
      text.style.zIndex = "3"
      test[4].style.zIndex = "1";
      test2[4].style.zIndex="-2";
+     liste[0].style.opacity="1";
+     liste[4].style.opacity="0.7";
    } else {
     index++;
     image = test[index];
@@ -23,39 +26,89 @@ function leTest(){
     text.style.zIndex = "3";
     test[index-1].style.zIndex = "1";
     test2[index-1].style.zIndex = "-2";
+    liste[index].style.opacity="1";
+    liste[index-1].style.opacity="0.7";
+
    }
  }
 
- var liste = document.getElementsByTagName('li');
 
  for (i = 0; i<liste.length;i++) {
-   liste[i].addEventListener("click", changeZIndex(this));
+   liste[i].addEventListener("click", function () {changeZIndex(this);});
  }
 
 function changeZIndex(th) {
-  alert("test");
- var g = document.getElementsByTagName('ul')[0];
- for (var i = 0, len = g.children.length; i < len; i++) {
-   (function(index){
-         g.children[i].onclick = function(){
-               alert(index)  ;
-         }
-     });
+  var a = th.id;
 
- }
-}
-/*
-function changeZIndex(th) {
-  var a = liste.indexOf(th);
-  alert(a);
 
-/*  switch (liste) {
-    case liste[0]:
-      // alert("ça marche");
+  console.log(a);
+
+  switch (a) {
+    case "0":
+      for (i=0; i < test.length; i++) {
+        if (i==0) {
+          test[0].style.zIndex = "2";
+          test2[0].style.zIndex = "3";
+        } else {
+          test[i].style.zIndex ="1";
+          test2[i].style.zIndex ="-2";
+        }
+      }
       break;
+
+    case "1":
+      for (i=0; i < test.length; i++) {
+        if (i==1) {
+          test[1].style.zIndex = "2";
+          test2[1].style.zIndex = "3";
+        } else {
+          test[i].style.zIndex ="1";
+          test2[i].style.zIndex ="-2";
+        }
+      }
+      break;
+
+    case "2":
+      for (i=0; i < test.length; i++) {
+        if (i==2) {
+          test[2].style.zIndex = "2";
+          test2[2].style.zIndex = "3";
+        } else {
+          test[i].style.zIndex ="1";
+          test2[i].style.zIndex ="-2";
+        }
+      }
+
+      break;
+
+    case "3":
+      for (i=0; i < test.length; i++) {
+        if (i==3) {
+          test[3].style.zIndex = "2";
+          test2[3].style.zIndex = "3";
+        } else {
+          test[i].style.zIndex ="1";
+          test2[i].style.zIndex ="-2";
+        }
+      }
+
+      break;
+
+    case "4":
+      for (i=0; i < test.length; i++) {
+        if (i==4) {
+          test[4].style.zIndex = "2";
+          test2[4].style.zIndex = "3";
+        } else {
+          test[i].style.zIndex ="1";
+          test2[i].style.zIndex ="-2";
+        }
+      }
+
+      break;
+
     default:
 
   }
-  */
+
 }
-*/
